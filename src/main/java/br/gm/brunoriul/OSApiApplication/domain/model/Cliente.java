72a0ad2dd788b43213a51ddadbad09d2a6b6be38
Bruno Riul <brunoc.riul@gmail.com>
2026-03-23 17:07:21 -0300

    3° commit - Implementação de Mapeamento Objeto-Relacional na classe ClienteAdicionando: Dependências Spring Data JPA e Driver MySQL no arquivo pom.xml.Modificando: Arquivo application.properties com configurações de conexão JDBC e credenciais do banco de dados.Modificando: Classe Cliente com as anotações jakarta.persistence (@Entity, @Id, @GeneratedValue e @Column) para persistência.Endpoint add: N/A (Persistência configurada no modelo, lógica de acesso a dados pendente de finalização).

--- src/main/java/br/gm/brunoriul/OSApiApplication/domain/model/Cliente.java
+++ src/main/java/br/gm/brunoriul/OSApiApplication/domain/model/Cliente.java
@@ -4,20 +4,28 @@
  */
 package br.gm.brunoriul.OSApiApplication.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 /**
  *
  * @author digma
  */
+@Entity
 public class Cliente {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
     private String email;
     private String fone;
-    
+
     public Cliente() {
     }
-    
+
     public Cliente(long id, String nome, String email, String fone) {
         this.id = id;
         this.nome = nome;
@@ -78,11 +86,5 @@ public class Cliente {
         final Cliente other = (Cliente) obj;
         return this.id == other.id;
     }
-    
-    
 
-    
-    
-    
-    
 }
