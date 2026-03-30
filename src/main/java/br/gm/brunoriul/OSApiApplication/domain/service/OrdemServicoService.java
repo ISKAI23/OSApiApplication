6bdfd8e1a85dd137a268d36ab82e5b214d2aeb4a
Bruno Riul <brunoc.riul@gmail.com>
2026-03-30 17:14:28 -0300

    sla só commitandopra ir embora

---
+++ src/main/java/br/gm/brunoriul/OSApiApplication/domain/service/OrdemServicoService.java
@@ -0,0 +1,28 @@
+package br.gm.brunoriul.OSApiApplication.domain.service;
+
+import br.gm.brunoriul.OSApiApplication.domain.model.OrdemServico;
+import br.gm.brunoriul.OSApiApplication.domain.model.StatusOrdemServico;
+import br.gm.brunoriul.OSApiApplication.domain.repository.OrdemServicoRepository;
+import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author digma
+ */
+
+@Service
+public class OrdemServicoService {
+    
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+    
+    public OrdemServico criar(OrdemServico ordemServico) {
+        ordemServico.setStatus(StatusOrdemServico.ABERTA);
+        ordemServico.setDataAbertura(LocalDateTime.now());
+        
+        return ordemServicoRepository.save(ordemServico);
+    }
+    
+}
